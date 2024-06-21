28ad6dff6b46da151e5e56f6ae54bcec6500e99d
dattu9133 <dattasaimoganti@gmail.com>
2024-06-21 11:22:43 +0530

    Add dynamic programming solution for coin change problem

---
+++ Dynamic Programming/CoinsChange.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CoinsChange {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number:");
+    int N = sc.nextInt();
+    System.out.println("Enter the no. of coins:");
+    int n = sc.nextInt();
+    int coins[] = new int[n];
+    System.out.println("Enter the coins:");
+    for (int i = 0; i < n; i++) {
+      coins[i] = sc.nextInt();
+    }
+    int res = minCoins(coins, n, N);
+    if (res == Integer.MAX_VALUE) {
+      System.out.println("No solution possible.");
+    } else {
+      System.out.println("Minimum number of coins: " + res);
+    }
+    sc.close();
+  }
+
+  private static int minCoins(int[] coins, int n, int N) {
+    if (N == 0)
+      return 0;
+    int dp[] = new int[N + 1];
+    for (int i = 1; i <= N; i++) {
+      dp[i] = Integer.MAX_VALUE;
+    }
+    dp[0] = 0;
+
+    for (int i = 1; i <= N; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i >= coins[j] && dp[i - coins[j]] != Integer.MAX_VALUE) {
+          dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+        }
+      }
+    }
+
+    return dp[N];
+  }
+}
