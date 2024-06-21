939589f2898ee5f66d38b6ae0a02178128bc4d41
dattu9133 <dattasaimoganti@gmail.com>
2024-06-21 12:11:07 +0530

    Add dynamic programming solution for finding the minimum number of perfect squares as sum

---
+++ Dynamic Programming/MinNoOfPerfectSquaresAsSum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MinNoOfPerfectSquaresAsSum {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number:");
+    int n = sc.nextInt();
+    int res = minPerfectSquares(n);
+    System.out.println("Minimum No. of Perfect Squares: " + res);
+    sc.close();
+  }
+
+  private static int minPerfectSquares(int n) {
+    int dp[] = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+      dp[i] = Integer.MAX_VALUE;
+      for (int j = 1; j * j <= i; j++) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+      }
+    }
+    return dp[n];
+  }
+}
